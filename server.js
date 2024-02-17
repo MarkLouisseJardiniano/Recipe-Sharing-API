b3fd7dd686051d95a621fe2b558654a1422e87e5
Mark Louisse Jardiniano <112992310+MarkLouisseJardiniano@users.noreply.github.com>
2024-02-17 10:29:34 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -5,8 +5,67 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-// Sample data  store recipes in an array
-const recipes = require('./data/recipes')
+// Sample data: store recipes and users in an array
+const recipes = require('./data/recipes');
+const users = [];
+
+
+// Routes
+app.get('/register', (req, res) => {
+    res.json(users);
+});
+
+// Register a new user
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if the username is already taken
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(400).json({ message: 'Username already taken' });
+    }
+
+    // Create a new user object
+    const newUser = {
+        id: users.length + 1,
+        username,
+        password 
+    };
+
+    // Add the new user to the array
+    users.push(newUser);
+
+    // It will redirect to login
+    res.redirect('http://localhost:3000/login');
+});
+
+
+app.get('/login', (req, res) => {
+    res.json(users);
+});
+
+
+// Login endpoint
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Find user by username
+    const user = users.find(user => user.username === username);
+
+    // Check if the user exists
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the password matches
+    if (user.password !== password) {
+        return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Return user information
+    res.redirect('http://localhost:3000/recipes');
+});
+
 
 // Get all recipes
 app.get('/recipes', (req, res) => {
@@ -16,7 +75,7 @@ app.get('/recipes', (req, res) => {
 // Get a specific recipe by ID or tags
 app.get('/recipes/:query', (req, res) => {
     const query = req.params.query.toLowerCase();
-    const recipe = recipes.filter((recipe) => recipe.id === parseInt(query) || recipe.tags.toLowerCase() === query);
+    const recipe = recipes.filter((recipe) => recipe.id === parseInt(query) || recipe.tags.toLowerCase() === (query) || recipe.title.toLowerCase() === query);
 
     if (!recipe.length) {
         return res.status(404).json({ message: 'This recipe does not exist' });
@@ -48,6 +107,7 @@ app.put('/recipes/:id', (req, res) => {
     if (index === -1) {
         return res.status(404).json({ message: 'This recipe does not exist' });
     }
+    
     recipes[index].title = title;
     recipes[index].ingredients = ingredients;
     recipes[index].cookingStep = cookingStep;
@@ -72,4 +132,3 @@ app.delete('/recipes/:id', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
